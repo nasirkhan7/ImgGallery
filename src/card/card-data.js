45c7b59579a4ddfe9d159fd6353616ba44d1228b
nasirkhan <nasirkhana842@gmail.com>
2023-12-17 13:27:19 -0500

    ImgGallery

---
+++ src/card/card-data.js
@@ -0,0 +1,82 @@
+export const card_data = [
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-1.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-2.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-3.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-4.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-5.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://images.unsplash.com/photo-1550586678-f7225f03c44b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8UGFraXN0YW58ZW58MHx8MHx8fDA%3D',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-7.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-8.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-9.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-10.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-11.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-12.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-13.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-14.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-15.jpg',
+        link: ''
+    },
+    {
+        id: 1,
+        img: 'https://bootstrapmade.com/demo/templates/PhotoFolio/assets/img/gallery/gallery-16.jpg',
+        link: ''
+    },
+]
